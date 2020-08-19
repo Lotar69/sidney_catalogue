bbe515faa2d7f9c4f307b794df63fee304e771c3
adelaide laanatri <laanatri.ade@gmail.com>
2020-08-19 22:02:38 +0200

    front back dashboard seller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController
   skip_before_action :authenticate_buyer!
   before_action :authenticate_seller!
+
+  def show
+    @animals = Animal.all
+  end
 end
