7c3e27baf3200f27479f64092671878c5c1bd6da
Loic Tardy <lotardy@gmail.com>
2020-08-19 18:08:43 +0200

    home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_buyer!
   def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :buyers
   devise_for :sellers
-  root    to: 'pages#home'
+  root    to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :animals, only: [:index, :show, :new, :create, :edit, :update]
 
